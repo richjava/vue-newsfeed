dd8ae45086cd4ca038ff0d308233f488b57ce81e
Richard Lovell <rjlovell@hotmail.com>
2019-01-22 13:11:07 +1300

    Formatting changes

--- src/main.js
+++ src/main.js
@@ -1,37 +1,38 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
-import VueFire from 'vuefire'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import VueResource from "vue-resource";
+import VueFire from "vuefire";
 
-import App from './App'
+import App from "./App";
 
-Vue.use(VueRouter)
-Vue.use(VueResource)
-Vue.use(VueFire)
+Vue.use(VueRouter);
+Vue.use(VueResource);
+Vue.use(VueFire);
 
-import Hello from './components/Hello'
-import About from './components/About'
-import Login from './components/Login'
-import Admin from './components/Admin'
-import Posts from './components/Posts'
-import Comments from './components/Comments'
+import Hello from "./components/Hello";
+import About from "./components/About";
+import Login from "./components/Login";
+import Admin from "./components/Admin";
+import Posts from "./components/Posts";
+import Comments from "./components/Comments";
 
 const routes = [
-  { path: '/', component: Hello },
-  { path: '/about', component: About },
-  { path: '/login', component: Login },
+  { path: "/", component: Hello },
+  { path: "/about", component: About },
+  { path: "/login", component: Login },
   {
-    path: '/admin', component: Admin,
+    path: "/admin",
+    component: Admin,
     children: [
       {
-        path: '/',
+        path: "/",
         component: Posts
       },
       {
-        path: 'comments',
+        path: "comments",
         component: Comments
       }
     ]
@@ -40,12 +41,12 @@ const routes = [
 
 const router = new VueRouter({
   routes,
-  mode: 'history'
-})
+  mode: "history"
+});
 
 new Vue({
-  el: '#app',
-  template: '<App/>',
+  el: "#app",
+  template: "<App/>",
   components: { App },
   router
-}).$mount('#app')//mount the router on the app
+}).$mount("#app"); //mount the router on the app
